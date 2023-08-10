2312c1d015966d9cd5216c2ac6256129edef489e
Muhammad Aiman Hakim bin Azahari <aimanhakimcy@gmail.com>
2023-08-10 17:17:01 +0800

    technical-test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'technical-test';
+  inputText = '';
+  convertedText = '';
+  firstWord = '';
+  secondWord = '';
+  wordPresentOutput = '';
+  triangleNumber = 0;
+  triangleOutput = '';
+
+  convertLetter() {
+    if (this.inputText.trim() === '') {
+      this.convertedText = 'Please enter a value.';
+      return;
+    }
+
+    let newLetter = '';
+
+    for (let i = 0; i < this.inputText.length; i++) {
+      const char = this.inputText[i];
+
+      if (/[a-zA-Z]/.test(char)) {
+        if (char === 'z') {
+          newLetter += 'a';
+        } else if (char === 'Z') {
+          newLetter += 'A';
+        } else {
+          newLetter += String.fromCharCode(char.charCodeAt(0) + 1);
+        }
+      } else {
+        newLetter += char;
+      }
+    }
+
+    this.convertedText = newLetter;
+  }
+
+  checkWordPresent() {
+    this.wordPresentOutput = this.isAllLettersPresent()
+      ? 'True! All letters are present.'
+      : 'False! Not all letters are present.';
+  }
+
+  isAllLettersPresent(): boolean {
+    for (let i = 0; i < this.secondWord.length; i++) {
+      if (this.firstWord.indexOf(this.secondWord[i]) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  calculateTriangle() {
+    const result = this.triangleNumberSequence(this.triangleNumber);
+    this.triangleOutput = `Corresponding number of dots : ${result}`;
+  }
+
+  triangleNumberSequence(n: number): number {
+    return n * (n + 1) / 2;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
